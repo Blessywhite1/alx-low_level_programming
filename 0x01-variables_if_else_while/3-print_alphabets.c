66e4db5793461c96640d23cb13836dd0dd4189c7
Blessywhite1 <chiwendub1@gmail.com>
2023-02-17 08:36:01 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,8 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);	
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)putchar(letter);	
+	for (letter = 'A'; letter <= 'Z'; letter++)putchar(letter);
 
 	putchar('\n');
 

1e45200478a2914fc49106d2c746a301a1ce3d03
Blessywhite1 <chiwendub1@gmail.com>
2023-02-17 23:08:21 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Point print single digits numbers of base 10 start from 1
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
